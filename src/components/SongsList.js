beda8e96ce8ceb914b59b7335223d6e9ea9c19f0
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-03-30 09:32:57 +0530

    Add files via upload

---
+++ src/components/SongsList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {selectSong} from '../actions';
+class SongsList extends React.Component{
+    renderList(){
+        return this.props.songs.map((songs)=>{
+            return (
+                <div className='item' key={songs.title}>
+                    <div className='right floated content'>
+                        <button onClick={()=> this.props.selectSong(songs)} className="ui button primary">Select</button>
+                    </div>
+                    <div className="content">
+                        {songs.title}
+                    </div>
+                </div>
+            );
+        })
+    }
+    render(){
+        
+        return(
+            <div className="ui divided list">
+                {this.renderList()}
+            </div>
+        );
+    }
+}
+const mapStateToProps = (state)=>{
+    return {songs:state.songs};    
+}
+export default connect(mapStateToProps,{selectSong})(SongsList);
